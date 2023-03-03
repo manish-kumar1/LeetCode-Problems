4c216056f3d10b8de8a8504578a3cd2c92435da5
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-03-03 11:25:12 +0530

    Nothing to commit

---
+++ Basic Problem/MultipleOfArrayElement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int solve(int arr[], int n){
+    int mul = 1;
+    for(int i = 0; i < n; i++){
+        mul = mul * arr[i];
+    }
+    return mul;
+}
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int ans = solve(arr, n);
+    cout << ans;
+
+    return 0;
+}
