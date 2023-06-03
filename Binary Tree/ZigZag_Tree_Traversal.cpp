f2bafbe6912d70d70f45a9b8ca7d14e8362db6e9
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-03 11:19:24 +0530

    ZigZag Tree Traversal ❤️🌹😎

---
+++ Binary Tree/ZigZag_Tree_Traversal.cpp
@@ -0,0 +1,181 @@
+/*
+Q. ZigZag Tree Traversal
+
+Given a Binary Tree. Find the Zig-Zag Level Order Traversal of the Binary Tree.
+
+Example 1:
+
+Input:
+        1
+      /   \
+     2     3
+    / \   /  \
+   4   5 6    7
+Output:
+1 3 2 4 5 6 7
+Example 2:
+
+Input:
+           7
+        /     \
+       9       7
+     /  \     /   
+    8    8   6     
+   /  \
+  10   9 
+Output:
+7 7 9 8 8 6 9 10 
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function zigZagTraversal() which takes the root node of the Binary Tree as its input and returns a list containing the node values as they appear in the Zig-Zag Level-Order Traversal of the Tree.
+
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= N <= 104
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+class Solution{
+    public:
+    vector<int> zigZagTraversal(Node* root){
+    	// Code here
+    	vector<int> result;
+    	if(root == NULL)
+    	    return result;
+    	
+    	queue<Node*> q;
+    	q.push(root);
+    	
+    	bool leftToRight = true;
+    	
+    	while(!q.empty()) {
+    	    
+    	    int size = q.size();
+    	    vector<int> ans(size);
+    	    
+    	    //Level Process
+    	    for(int i=0; i<size; i++) {
+    	        
+    	        Node* frontNode = q.front();
+    	        q.pop();
+    	        
+    	        //normal insert or reverse insert 
+    	        int index = leftToRight ? i : size - i - 1;
+    	        ans[index] = frontNode -> data;
+    	        
+    	        if(frontNode->left)
+    	            q.push(frontNode -> left);
+    	            
+    	        if(frontNode->right)
+    	            q.push(frontNode -> right);
+    	    }
+    	    
+    	    //direction change karni h
+    	    leftToRight = !leftToRight;
+    	   
+    	  for(auto i: ans) {
+    	      result.push_back(i);
+    	  }  
+    	}
+    	return result;
+    }
+};
+
+int main() {
+
+    string s, ch;
+    getline(cin, s);
+    
+    Node* root = buildTree(s);
+    Solution ob;
+    vector<int> ans = ob.zigZagTraversal(root);
+
+    cout << "Output = ";
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
