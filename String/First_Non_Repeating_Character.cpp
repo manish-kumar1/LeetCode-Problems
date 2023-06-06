0d0afe73fcbe860125e44b6f302b0fd5669f340c
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-06 19:16:22 +0530

    First Non Repeating Character Deloitee Interview Experience❤️

---
+++ String/First_Non_Repeating_Character.cpp
@@ -0,0 +1,59 @@
+/*
+Q. First Non Repeating Character
+
+Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+
+Example 1:
+
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+Example 2:
+
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. 
+Your Task:
+You only need to complete the function nonrepeatingCharacter() that takes string S as a parameter and returns the character. If there is no non-repeating character then return '$' .
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+Note: N = |S|
+
+Constraints:
+1 <= N <= 105
+*/
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+char nonRepeating(string s){
+
+    unordered_map<char, int> mp;
+    for(int i = 0; i < s.size(); i++){
+        mp[s[i]]++;
+    }
+    for(int i = 0; i < s.size(); i++){
+        if(mp[s[i]] == 1) {
+            return s[i];
+        }
+    }
+    return -1;
+}
+int main(){
+
+    string s;
+    cin >> s;
+
+    char ans = nonRepeating(s);
+    cout << ans;
+
+    return 0;
+}
