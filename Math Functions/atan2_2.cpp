bcf9287a8ade59ee008ee9118d3afc8067b532f1
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-15 21:53:12 +0530

    h

---
+++ Math Functions/atan2_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+#include<math.h>  
+using namespace std;  
+int main()  
+{  
+    int x=6;  
+    float y=7.8;  
+    cout<<"Value of tan(y/x) is : "<<tan(y/x)<<'\n';  
+    std::cout << "Value of tan-1(y/x) is : " <<atan2(y,x)<< std::endl;  
+    return 0;  
+}  
