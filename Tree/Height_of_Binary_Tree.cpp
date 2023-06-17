f328f4f6970eb16ab9b0d1c1a3d4dd372af45399
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-18 00:20:52 +0530

    Height of Binary Tree

---
+++ Tree/Height_of_Binary_Tree.cpp
@@ -0,0 +1,105 @@
+/*
+Q. Height of Binary Tree
+
+Given a binary tree, find its height.
+
+Example 1:
+
+Input:
+     1
+    /  \
+   2    3
+
+Output: 2
+
+Example 2:
+
+Input:
+  2
+   \
+    1
+   /
+ 3
+Output: 3   
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function height() which takes root node of the tree as input parameter and returns an integer denoting the height of the tree. If the tree is empty, return 0. 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 109
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+Node* buildTree(string str){
+    if(str.length() == 0 || str[0] == 'N'){
+        return NULL;
+    }
+    vector<string> ip;
+    istringstream iss(str);
+    for(string str; iss >> str;){
+        ip.push_back(str);
+    }
+
+    Node *root = new Node(stoi(ip[0]));
+
+    queue<Node*> queue;
+    queue.push(root);
+
+    int i = 1;
+    while(!queue.empty() && i < ip.size()){
+        Node* currNode = queue.front();
+        queue.pop();
+
+        string currVal = ip[i];
+
+        if(currVal != "N"){
+            currNode->left = new Node(stoi(currVal));
+            queue.push(currNode->left);
+        }
+        i++;
+        if(i >= ip.size()){
+            break;
+        }
+        currVal = ip[i];
+        if(currVal != "N"){
+            currNode->right = new Node(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+int height(struct Node* node){
+    if(node == NULL){
+        return 0;
+    }
+    int left = height(node -> left);
+    int right = height(node -> right);
+    int ans = max(left, right) + 1;
+
+    return ans;
+}
+int main(){
+
+    string treeString;
+    getline(cin, treeString);
+    Node* root = buildTree(treeString);
+
+    int ans = height(root);
+
+    return 0;
+}
