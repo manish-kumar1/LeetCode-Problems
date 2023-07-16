9f033b7742a2697070838342f3da19f664a67984
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-07-16 10:47:57 +0530

    kj

--- Bit Manipulation/Counting_Bits.cpp
+++ Bit Manipulation/Counting_Bits.cpp
@@ -41,8 +41,9 @@ using namespace std;
 vector<int> countBits(int n){
 
     vector<int> ans(n+1, 0);
-    
+
     for(int i = 1; i <= n; i++){
+        
         ans[i] = ans[i >> 1] + (i & 1);
     }
     return ans;
