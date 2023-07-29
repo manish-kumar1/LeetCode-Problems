5ad25182eb09db7a42dff884f2e3e86a8cb129dc
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-07-29 11:07:36 +0530

    dfgh

--- Bit Manipulation/Missing_Number.cpp
+++ Bit Manipulation/Missing_Number.cpp
@@ -7,6 +7,7 @@ return the only number in the range that is missing from the array.
 Example 1:
 Input: nums = [3,0,1]
 Output: 2
+
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
 Example 2:
