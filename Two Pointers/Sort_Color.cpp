47f499ffaa77036ee6ebdad45e73c21929d18afc
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-08-08 23:02:07 +0530

    Sort Color or 0 1 2 sort Leetcode problem using two pointer 😂😂❤️❤️🚩

--- Two Pointers/Sort_Color.cpp
+++ Two Pointers/Sort_Color.cpp
@@ -36,6 +36,7 @@ vector<int> sortColors(vector<int> &nums){
     int start = 0;
     int mid = 0;
     int end = nums.size()-1;
+    
     while(mid <= end){
         if(nums[mid] == 0){
             swap(nums[mid], nums[start]);
