f046e9e30b0acc2714d7c738ff6e66c6ffe64d13
Manish Kumar <manishkumar40105@gmail.com>
2023-10-16 00:15:25 +0530

    LeetCode Problem

---
+++ Array/Single_Element_in_Sorted_Array.cpp
@@ -0,0 +1,50 @@
+/*
+Q. Single Element in Sorted Array
+
+You are given a sorted array consisting of only integers where every element appears exactly twice, 
+except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+ 
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+Example 2:
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+ 
+Constraints:
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int singleElement(vector<int> &nums){
+    int ans = 0;
+    for(int i = 0; i < nums.size(); i++){
+        ans = ans ^ nums[i];
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cin >> n;
+
+    int v = 0;
+    vector<int> nums;
+    for(int i = 0; i < n; i++){
+        cin >> v;
+        nums.push_back(v);
+    }
+    int ans = singleElement(nums);
+    cout << ans;
+
+    return 0;
+}
