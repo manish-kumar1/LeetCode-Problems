d1979d6a5a8404a6c7d90ad81cffd11ad736cee0
Manish Kumar <manishkumar40105@gmail.com>
2023-11-09 21:03:54 +0530

    LeetCode

---
+++ Database/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,122 @@
+/*
+Q. Customer Who Visited but Did Not Make Any Transactions
+
+Table: Visits
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+Table: Transactions
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+Return the result table sorted in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+Output: 
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
+Explanation: 
+Customer with id = 23 visited the mall once and made one transaction during the visit with id = 12.
+Customer with id = 9 visited the mall once and made one transaction during the visit with id = 13.
+Customer with id = 30 visited the mall once and did not make any transactions.
+Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
+Customer with id = 96 visited the mall once and did not make any transactions.
+As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+*/
+-- TABLE VISITS
+
+CREATE TABLE VISITS(
+			VISIT_ID INT,
+            CUSTOMER_ID INT
+);
+
+INSERT INTO VISITS
+(VISIT_ID, CUSTOMER_ID)
+VALUES
+(1, 23),
+(2, 9),
+(4, 30),
+(5, 54),
+(6, 96),
+(7, 54),
+(8, 54);
+
+SELECT *FROM VISITS;
+
+-- TRANSACTIONS
+
+CREATE TABLE TRANSACTIONS(
+			TRANSACTION_ID INT,
+			VISIT_ID INT,
+            AMOUNT INT
+);
+
+INSERT INTO TRANSACTIONS
+(TRANSACTION_ID, VISIT_ID, AMOUNT)
+VALUES
+(2, 5, 310),
+(3, 5, 300),
+(9, 5, 200),
+(12, 1, 910),
+(13, 2, 970);
+
+SELECT *FROM TRANSACTIONS;
+
+-- ACOORDING TO QUESTION
+
+SELECT V.CUSTOMER_ID, COUNT(V.VISIT_ID) AS COUNT_NO_TRANS
+FROM VISITS AS V
+LEFT JOIN TRANSACTIONS AS T
+ON V.VISIT_ID = T.VISIT_ID
+WHERE T.TRANSACTION_ID IS NULL
+GROUP BY  V.CUSTOMER_ID;
+
+
+
+
+
