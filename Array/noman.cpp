71a3716d87277416144b07c9b6d780903342cc0c
manish-kumar1 <manishkumar40105@gmail.com>
2023-11-11 13:12:21 +0530

    noman commit

---
+++ Array/noman.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int v = 0;
+    vector<int> nums;
+    for(int i = 0; i < n; i++){
+        cin >> v;
+        nums.push_back(v);
+        // Hello noman
+        
+    }
+
+
+    return 0;
+}
