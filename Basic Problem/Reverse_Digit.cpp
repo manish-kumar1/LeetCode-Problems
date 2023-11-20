9de73d4789f251cacab6c51112f6936071aa302c
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-11-20 22:05:14 +0530

    when hole day not do code on vs code but want to save github streak

---
+++ Basic Problem/Reverse_Digit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    int sum = 0;
+    while(n != 0){
+        int digit = n%10;
+        sum = sum*10 + digit;
+        n /= 10;
+    }
+    return sum;
+}
+int main(){
+
+    int n;
+    cin >> n;
+
+    int ans = sum(n);
+    cout << ans;
+
+    return 0;
+}
