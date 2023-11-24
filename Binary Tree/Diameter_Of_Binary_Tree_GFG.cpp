995ba7c675d30832a4c903b8260ae69fcab05230
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-11-24 18:13:28 +0530

    LeetCode and GeeksForGeeks problems of Binary Tree for FNAAG

---
+++ Binary Tree/Diameter_Of_Binary_Tree_GFG.cpp
@@ -0,0 +1,156 @@
+/*
+Diameter of Binary Tree
+
+The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two end nodes. The diagram below shows two trees each with diameter nine, the leaves that form the ends of the longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes). 
+
+
+Example 1:
+
+Input:
+       1
+     /  \
+    2    3
+
+Output: 3
+
+Example 2:
+
+Input:
+         10
+        /   \
+      20    30
+    /   \ 
+   40   60
+
+Output: 4
+
+Your Task:
+You need to complete the function diameter() that takes root as parameter and returns the diameter.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= Number of nodes <= 10000
+1 <= Data of a node <= 1000
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+class Solution{
+    private:
+    pair<int, int> DiameterFast(Node* root){
+        if(root == NULL){
+            pair<int, int> p = make_pair(0, 0);
+            return p;
+        }
+        pair<int, int> left = DiameterFast(root->left);
+        pair<int, int> right = DiameterFast(root->right);
+
+        int opt1 = left.first;
+        int opt2 = right.first;
+        int opt3 = left.second + right.second + 1;
+
+        pair<int, int> ans;
+        ans.first = max(opt1, max(opt2, opt3));
+        ans.second = max(left.second, right.second) + 1;
+
+        return ans;
+    }
+    public:
+    int DiameterOfTree(Node* root){
+        return DiameterFast(root).first;
+    }
+};
+int main(){
+
+    string s;
+    getline(cin, s);
+
+    Node* root = buildTree(s);
+
+    Solution obj;
+    cout << "Output : ";
+    cout <<obj.DiameterOfTree(root);
+
+    return 0;
+}
---
+++ Binary Tree/Path_Sum.cpp
@@ -0,0 +1,143 @@
+/*
+Q. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+
+Input: root = [5,4,8,11,N,13,4,7,2,N,N,N,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+class Solution{
+    public:
+    bool PathSum(Node* root, int targetSum){
+        if(root == NULL){
+            return false;
+        }
+        if(root->left == NULL and root->right == NULL){
+            return targetSum == root->data;
+        }
+        return PathSum(root->left, targetSum-root->data) or PathSum(root->right, targetSum-root->data);
+    }
+
+};
+int main(){
+
+    string s;
+    getline(cin, s);
+
+    Node* root = buildTree(s);
+    Solution obj;
+
+    int targetSum; cin >> targetSum;
+    cout << "Output : ";
+    cout <<obj.PathSum(root, targetSum);
+
+    return 0;
+}
