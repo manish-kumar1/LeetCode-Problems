405e390f31918755ff9f28d8e0922ec4d9c8bb29
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-12-10 15:57:45 +0530

    Transpose Matrix

---
+++ Matrix/Transpose_Matrix.cpp
@@ -0,0 +1,58 @@
+/*
+Q. Transpose Matrix
+
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+void TransposeMatrix(vector<vector<int>> &matrix, int n){
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            matrix[i][j] = matrix[j][i];
+        }
+    }
+}
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    TransposeMatrix(matrix, n);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
