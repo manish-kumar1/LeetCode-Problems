26e4aacdaea25214ffee23f9fe851003f948218d
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-07 14:45:28 +0530

    M

---
+++ OOPs/Object_counting.cpp
@@ -0,0 +1,33 @@
+#include <iostream>  
+using namespace std;  
+
+class Account {  
+   public:  
+       int accno; //data member (also instance variable)      
+       string name;   
+       static int count;     
+       Account(int accno, string name)   
+        {    
+            
+        }    
+       void display()    
+        {    
+            cout<<accno<<" "<<nathis->accno = accno;    
+            this->name = name;    
+            count++;  me<<endl;   
+        }    
+};  
+int Account::count=0; 
+
+int main(void) {  
+    Account a1 =Account(201, "Sanjay"); //creating an object of Account  
+    Account a2=Account(202, "Nakul");   
+    Account a3=Account(203, "Ranjana");  
+
+    a1.display();    
+    a2.display();    
+    a3.display();    
+
+    cout<<"Total Objects are: "<<Account::count;  
+    return 0;  
+}  
