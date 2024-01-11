cb875b94541008e35f4cd58cde0cc4dde097e0a7
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-11 12:36:10 +0530

    Amount of time for Binary Tree to be Infected LeetCode POTD❤️❤️❤️❤️❤️

---
+++ Binary Tree/Amount_of_Time_for_Binary_Tree_to_Be_Infected.cpp
@@ -0,0 +1,183 @@
+/*
+Q. Amount of Time for Binary Tree to be Infected
+
+You are given the root of a binary tree with unique values, and an integer start. At minute 0, an infection starts from the node with value start.
+
+Each minute, a node becomes infected if:
+
+The node is currently uninfected.
+The node is adjacent to an infected node.
+Return the number of minutes needed for the entire tree to be infected.
+
+Example 1:
+
+Input: root = [1,5,3,null,4,10,6,9,2], start = 3
+Output: 4
+Explanation: The following nodes are infected during:
+- Minute 0: Node 3
+- Minute 1: Nodes 1, 10 and 6
+- Minute 2: Node 5
+- Minute 3: Node 4
+- Minute 4: Nodes 9 and 2
+It takes 4 minutes for the whole tree to be infected so we return 4.
+
+Example 2:
+
+Input: root = [1], start = 1
+Output: 0
+Explanation: At minute 0, the only node in the tree is infected so we return 0.
+ 
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 105
+Each node has a unique value.
+A node with a value of start exists in the tree.
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+class Solution{
+    public:
+    void convert(Node* current, int parent, unordered_map<int, vector<int>>& adj) {
+        if (current == nullptr) {
+            return;
+        }
+        if (parent != -1) {
+            adj[current->data].push_back(parent);
+        }
+
+        if (current->left != nullptr) {
+            adj[current->data].push_back(current->left->data);
+        } 
+        if (current->right != nullptr) {
+            adj[current->data].push_back(current->right->data);
+        }
+        convert(current->left, current->data, adj);
+        convert(current->right, current->data, adj);
+    }
+    int amountOfTime(Node* root, int start) {
+        unordered_map<int, vector<int>> adj;
+        convert(root, -1, adj);
+
+        queue<int> que;
+        que.push(start);
+        unordered_set<int> visited;
+        visited.insert(start);
+        int minutes = 0;
+
+        while (!que.empty()) {
+            int n = que.size();
+
+            while (n--) {
+                int curr = que.front();
+                que.pop();
+
+                for (int &ngbr : adj[curr]) {
+                    if (visited.find(ngbr) == visited.end()) {
+                        que.push(ngbr);
+                        visited.insert(ngbr);
+                    }
+                }
+            }
+            minutes++;
+        }
+        return minutes - 1;
+    }
+};
+
+int main() {
+
+    string s, ch;
+    getline(cin, s);
+    
+    Node* root = buildTree(s);
+    Solution ob;
+ 
+    int start;
+    cout << "start : "; cin >> start;
+
+    int ans = ob.amountOfTime(root, start);
+    
+    cout << "Output = " << ans;
+
+
+    return 0;
+}
