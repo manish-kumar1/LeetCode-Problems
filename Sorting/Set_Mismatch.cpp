95486a647c914250dce1957140f626ae4d72eb62
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-23 01:42:50 +0530

    Jay Shree RAM Jay Shree RAM 🚩🚩🚩🚩🚩🚩🚩🚩🚩🚩🚩

---
+++ Sorting/Set_Mismatch.cpp
@@ -0,0 +1,67 @@
+/*
+Q. Set Mismatch
+
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+1 <= nums[i] <= 104
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> setMismatch(vector<int> &nums){
+
+    int n = nums.size();
+    unordered_map<int, int> mp;
+    for(int i = 0; i < n; i++){
+        mp[nums[i]]++;
+    }
+    int duplicate;
+    for(int i = 0; i < n; i++){
+        if(mp[nums[i]] == 2){
+            duplicate = nums[i];
+            break;
+        }
+    }
+    int missingNumber;
+    int totalSum = accumulate(nums.begin(), nums.end(), 0);
+    int sum = n * (n+1) / 2;
+    totalSum = totalSum - duplicate;
+    missingNumber = sum - totalSum;
+    return {duplicate, missingNumber};
+}
+int main(){
+
+    int n;
+    cin >> n;
+
+    int temp = 0;
+    vector<int> nums;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        nums.push_back(temp);
+    }
+    vector<int> ans = setMismatch(nums);
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
