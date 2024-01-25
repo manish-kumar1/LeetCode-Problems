eab7756cd14415160005bf68888a4123ae5e2af3
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-25 23:44:03 +0530

    aaj noman ka birthday h par bho@@di ke party nahi diya h😂😂😂😂

--- Bit Manipulation/Reverse_Bits.cpp
+++ Bit Manipulation/Reverse_Bits.cpp
@@ -3,8 +3,6 @@ Q. Reverse Bits
 
 Reverse bits of a given 32 bits unsigned integer.
 
-Note:
-
 Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
 In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
  
@@ -50,7 +48,7 @@ int main(){
     cin >> n;
 
     uint32_t ans = reverseBits(n);
-    
+
     cout << ans << endl;
 
     return 0;
