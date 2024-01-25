c8a3b63710e019f99330c0270015a652c83a57c0
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-25 23:42:51 +0530

    Reverse Bit

--- Bit Manipulation/Reverse_Bits.cpp
+++ Bit Manipulation/Reverse_Bits.cpp
@@ -36,7 +36,7 @@ using namespace std;
 uint32_t reverseBits(uint32_t n){
 
     uint32_t ans = 0;
-    
+
     for(int i = 0; i < 32; i++){
         ans <<= 1;
         ans |= (n & 1);
@@ -50,6 +50,7 @@ int main(){
     cin >> n;
 
     uint32_t ans = reverseBits(n);
+    
     cout << ans << endl;
 
     return 0;
