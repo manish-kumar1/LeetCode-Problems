7640d0164f387ca91f218c8dc14be25e08d9b0a1
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-31 22:45:29 +0530

    Smallest Missing Positive Number GeeksforGeeks question❤️❤️❤️❤️😎

---
+++ Hash Table/Smallest_Missing_Positive_Number.cpp
@@ -0,0 +1,73 @@
+
+/*
+Q. Smallest Missing Positive Number
+
+You are given an array arr[] of N integers. The task is to find the smallest positive number missing from the array.
+
+Note: Positive number starts from 1.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {1,2,3,4,5}
+
+Output: 6
+Explanation: Smallest positive missing number is 6.
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {0,-10,1,3,-20}
+
+Output: 2
+Explanation: Smallest positive missing number is 2.
+
+Your Task:
+The task is to complete the function missingNumber() which returns the smallest positive missing number in the array.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 106
+-106 <= arr[i] <= 106
+*/
+
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int missingNumber(vector<int> &arr){
+
+    unordered_map<int, int> mp;
+    int ans = 0;
+    for(int i = 0; i < arr.size(); i++){
+        mp[arr[i]]++;
+    }
+    for(int i = 1; i <= arr.size()+1; i++){
+        if(mp[i] == 0){
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+}
+int main(){
+
+    int n;
+    cin >> n;
+
+    int temp = 0;
+    vector<int> arr;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    int ans = missingNumber(arr);
+    cout << "Output : " << ans; 
+
+    return 0;
+}
