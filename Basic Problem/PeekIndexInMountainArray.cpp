22a7799c36267c29930139e8a488d6bfc3e2fd82
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-02-22 23:18:56 +0530

    index in mountain array

--- Basic Problem/PeekIndexInMountainArray.cpp
+++ Basic Problem/PeekIndexInMountainArray.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 int indexInMountainArray(vector<int> &nums){
+
     int start = 0;
     int end = nums.size() - 1;
 
     while(start < end){
+        
         int mid = start + (end - start) / 2;
         if(nums[mid] < nums[mid+1]){
             start = mid + 1;
