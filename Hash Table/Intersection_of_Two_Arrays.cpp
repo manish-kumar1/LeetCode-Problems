a4c30638abc414707166a4a469c3df131b0dc188
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-03-15 23:48:30 +0530

    Intersection of Two Arrays

--- Hash Table/Intersection_of_Two_Arrays.cpp
+++ Hash Table/Intersection_of_Two_Arrays.cpp
@@ -24,10 +24,12 @@ Constraints:
 using namespace std;
 
 vector<int> solve(vector<int> &nums1, vector<int> &nums2){
+
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
     int i = 0, j = 0;
     set<int> st;
+    
     while(i < nums1.size() && j < nums2.size()){
         if(nums1[i] == nums2[j]){
             st.insert(nums1[i]);
