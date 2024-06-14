ea3e444e96ae37fccc423365277e2e3f42e1e8ae
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-06-14 22:11:11 +0530

    Remove Duplicates from sorted array

--- Array/Remove_Duplicates_Sorted_Array.cpp
+++ Array/Remove_Duplicates_Sorted_Array.cpp
@@ -27,6 +27,7 @@ If all assertions pass, then your solution will be accepted.
 Example 1:
 Input: nums = [1,1,2]
 Output: 2, nums = [1,2,_]
+
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
